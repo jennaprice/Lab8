a74e63319de26d6bc01ed0365560029e250bfa03
J.O.Price <notantisocial@users.noreply.github.com>
2017-11-01 11:18:52 -0400

    Made all the mathmatical calculations work

--- src/Batter.java
+++ src/Batter.java
@@ -1,11 +1,14 @@
+import java.util.Scanner;
 
 public class Batter {
 
+	private String name;
 	private double battingAverage;
 	private double sluggingPercentage;
 	private int[] atBatRecord;
 
 	public Batter(int numOfAtBats) {
+		name = "John Smith";
 		atBatRecord = new int[numOfAtBats];
 		sluggingPercentage = 0;
 		battingAverage = 0;
@@ -16,11 +19,26 @@ public class Batter {
 		atBatRecord = atBats;
 		sluggingPercentage = 0;
 		battingAverage = 0;
+		name = "John Smith";
 
 	}
 
-	public void recordBats(int[] atBats) {
-		atBatRecord = atBats;
+	public Batter(int atBats, String batterName) {
+		atBatRecord = new int[atBats];
+		sluggingPercentage = 0;
+		battingAverage = 0;
+		name = batterName;
+
+	}
+
+	public void recordBats(Scanner in) {
+
+		System.out.println("0 = out, 1 = single,2 = double, 3 = triple, 4 = home run");
+
+		for (int i = 0; i < atBatRecord.length; i++) {
+			System.out.println("Result for at-bat " + i + ":");
+			atBatRecord[i] = in.nextInt();// needs to be validated
+		}
 	}
 
 	public double calculateSluggingPercentage() {
@@ -28,9 +46,9 @@ public class Batter {
 		// calculate slugging percentage = total number of bases earned/ total number at
 		// bats
 		for (int i : atBatRecord) {
-			basesEarned = +i;// check the math
+			basesEarned = basesEarned + i;
 		}
-		sluggingPercentage = basesEarned / atBatRecord.length;
+		sluggingPercentage = basesEarned / (double) atBatRecord.length;
 		return sluggingPercentage;
 	}
 
@@ -40,11 +58,16 @@ public class Batter {
 		double totalAtBats = 0;
 		for (int i : atBatRecord) {
 			if (i != 0) {
-				totalAtBats = +i;
+				totalAtBats++;
 			}
+
 		}
-		battingAverage = totalAtBats / atBatRecord.length;
+		battingAverage = totalAtBats / (double) atBatRecord.length;
 		return battingAverage;
+	}
+
+	public String getName() {
+		return name;
 	}
 
 }
--- src/BattingAverageCalculator.java
+++ src/BattingAverageCalculator.java
@@ -11,11 +11,19 @@ public class BattingAverageCalculator {
 
 	public static void main(String[] args) {
 		int numOfBats;
+		String batterName;
 		// prompt users for bases earned
 
 		Scanner scnr = new Scanner(System.in);
+		System.out.println("Please enter your batter's name:");
+		batterName = scnr.nextLine();
 		System.out.println("Please enter the number of at bats you would like to calculate:");
 		numOfBats = scnr.nextInt();// needs validated
+		Batter batter1 = new Batter(numOfBats, batterName);
+		batter1.recordBats(scnr);
+		System.out.printf(batter1.getName() + "'s batting Average is %.3f", batter1.calculateBattingAverage());
+		System.out.println();
+		System.out.printf(batter1.getName() + "'s slugging Percentage is %.2f", +batter1.calculateSluggingPercentage());
 		scnr.close();
 
 	}
